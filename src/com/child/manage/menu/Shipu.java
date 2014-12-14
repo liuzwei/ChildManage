7aba050f7503b14d1fd592478d6bc73eaa815b31
long <826321978@qq.com>
2014-12-14 21:29:09 +0800

    界面

--- src/com/child/manage/menu/Shipu.java
+++ src/com/child/manage/menu/Shipu.java
@@ -41,49 +41,23 @@ public class Shipu {
 	private Context mContext;
 	private Activity mActivity;
 	private ChildApplication mKXApplication;
-	private View mHome;
-    private View mPopView;
-	private RelativeLayout mUgcLayout;
-	private ImageView mUgc;
-	private ImageView mUgcBg;
-	private ImageView mUgcVoice;
-	private ImageView mUgcPhoto;
-	private ImageView mUgcRecord;
-	private ImageView mUgcLbs;
+	private View mShipu;
 	private FlipperLayout.OnOpenListener mOnOpenListener;
-
-	private ListView mPopDisplay;
-
-	/**
-	 * 判断当前的path菜单是否已经显示
-	 */
-	private boolean mUgcIsShowing = false;
 
 	public Shipu(Context context, Activity activity, ChildApplication application) {
 		mContext = context;
 		mActivity = activity;
 		mKXApplication = application;
-		mHome = LayoutInflater.from(context).inflate(R.layout.shipu, null);
-		mPopView = LayoutInflater.from(context).inflate(
-				R.layout.home_popupwindow, null);
+		mShipu = LayoutInflater.from(context).inflate(R.layout.shipu, null);
+
 		findViewById();
 		setListener();
 
 	}
 
 	private void findViewById() {
-		mMenu = (Button) mHome.findViewById(R.id.home_menu);
-		mUgcView = (View) mHome.findViewById(R.id.home_ugc);
-		mUgcLayout = (RelativeLayout) mUgcView.findViewById(R.id.ugc_layout);
-		mUgc = (ImageView) mUgcView.findViewById(R.id.ugc);
+		mMenu = (Button) mShipu.findViewById(R.id.shipu_menu);
 
-		mUgcBg = (ImageView) mUgcView.findViewById(R.id.ugc_bg);
-		mUgcVoice = (ImageView) mUgcView.findViewById(R.id.ugc_voice);
-		mUgcPhoto = (ImageView) mUgcView.findViewById(R.id.ugc_photo);
-		mUgcRecord = (ImageView) mUgcView.findViewById(R.id.ugc_record);
-		mUgcLbs = (ImageView) mUgcView.findViewById(R.id.ugc_lbs);
-		mPopDisplay = (ListView) mPopView
-				.findViewById(R.id.home_popupwindow_display);
 
 	}
 
@@ -97,190 +71,12 @@ public class Shipu {
 			}
 		});
 
-		// Path监听
-		mUgcView.setOnTouchListener(new OnTouchListener() {
-
-			public boolean onTouch(View v, MotionEvent event) {
-				// 判断是否已经显示,显示则关闭并隐藏
-				if (mUgcIsShowing) {
-					mUgcIsShowing = false;
-					UgcAnimations.startCloseAnimation(mUgcLayout, mUgcBg, mUgc,
-                            500);
-					return true;
-				}
-				return false;
-			}
-		});
-		// Path监听
-		mUgc.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				// 判断是否显示,已经显示则隐藏,否则则显示
-				mUgcIsShowing = !mUgcIsShowing;
-				if (mUgcIsShowing) {
-					UgcAnimations.startOpenAnimation(mUgcLayout, mUgcBg, mUgc,
-							500);
-				} else {
-					UgcAnimations.startCloseAnimation(mUgcLayout, mUgcBg, mUgc,
-							500);
-				}
-			}
-		});
-		// Path 语音按钮监听
-		mUgcVoice.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				Animation anim = UgcAnimations.clickAnimation(500);
-				anim.setAnimationListener(new AnimationListener() {
-
-					public void onAnimationStart(Animation animation) {
-
-					}
-
-					public void onAnimationRepeat(Animation animation) {
-
-					}
-
-					public void onAnimationEnd(Animation animation) {
-						mContext.startActivity(new Intent(mContext,
-								VoiceActivity.class));
-						closeUgc();
-					}
-				});
-				mUgcVoice.startAnimation(anim);
-			}
-		});
-		// Path 拍照按钮监听
-		mUgcPhoto.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				Animation anim = UgcAnimations.clickAnimation(500);
-				anim.setAnimationListener(new AnimationListener() {
-
-					public void onAnimationStart(Animation animation) {
-
-					}
-
-					public void onAnimationRepeat(Animation animation) {
-
-					}
-
-					public void onAnimationEnd(Animation animation) {
-                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        File dir = new File("/sdcard/KaiXin/Camera/");
-                        if (!dir.exists()) {
-                            dir.mkdirs();
-                        }
-                        mKXApplication.mUploadPhotoPath = "/sdcard/KaiXin/Camera/"
-                                + UUID.randomUUID().toString();
-                        File file = new File(
-                                mKXApplication.mUploadPhotoPath);
-                        if (!file.exists()) {
-                            try {
-                                file.createNewFile();
-                            } catch (IOException e) {
-
-                            }
-                        }
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                                Uri.fromFile(file));
-                        mActivity
-                                .startActivityForResult(
-                                        intent,
-                                        ActivityForResultUtil.REQUESTCODE_UPLOADPHOTO_CAMERA);
-                        closeUgc();
-					}
-				});
-				mUgcPhoto.startAnimation(anim);
-			}
-		});
-		// Path 记录按钮监听
-		mUgcRecord.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				Animation anim = UgcAnimations.clickAnimation(500);
-				anim.setAnimationListener(new AnimationListener() {
-
-					public void onAnimationStart(Animation animation) {
-
-					}
-
-					public void onAnimationRepeat(Animation animation) {
-
-					}
-
-					public void onAnimationEnd(Animation animation) {
-						mContext.startActivity(new Intent(mContext,
-								WriteRecordActivity.class));
-						closeUgc();
-					}
-				});
-				mUgcRecord.startAnimation(anim);
-			}
-		});
-		// Path 签到按钮监听
-		mUgcLbs.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				Animation anim = UgcAnimations.clickAnimation(500);
-				anim.setAnimationListener(new AnimationListener() {
-
-					public void onAnimationStart(Animation animation) {
-
-					}
-
-					public void onAnimationRepeat(Animation animation) {
-
-					}
-
-					public void onAnimationEnd(Animation animation) {
-						mContext.startActivity(new Intent(mContext,
-								CheckInActivity.class));
-						closeUgc();
-					}
-				});
-				mUgcLbs.startAnimation(anim);
-			}
-		});
 	}
 
-	/**
-	 * 获取Path菜单显示状态
-	 * 
-	 * @return 显示状态
-	 */
-	public boolean getUgcIsShowing() {
-		return mUgcIsShowing;
-	}
 
-	/**
-	 * 关闭Path菜单
-	 */
-	public void closeUgc() {
-		mUgcIsShowing = false;
-		UgcAnimations.startCloseAnimation(mUgcLayout, mUgcBg, mUgc, 500);
-	}
-
-	/**
-	 * 显示Path菜单
-	 */
-	public void showUgc() {
-		if (mUgcView != null) {
-			mUgcView.setVisibility(View.VISIBLE);
-		}
-	}
-
-	/**
-	 * 关闭Path菜单
-	 */
-	public void dismissUgc() {
-		if (mUgcView != null) {
-			mUgcView.setVisibility(View.GONE);
-		}
-	}
 
 	public View getView() {
-		return mHome;
+		return mShipu;
 	}
 
 	public void setOnOpenListener(FlipperLayout.OnOpenListener onOpenListener) {
